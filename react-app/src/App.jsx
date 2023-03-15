4af613ad62cd4dd6b1f4125d814d1888daf3fc66
sadik312 <sadikislam46@gmail.com>
2023-03-15 20:42:19 +0000

    setup react app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as Pages from './pages';
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
+
+import './App.css';
+
+const App = () => {
+
+}
