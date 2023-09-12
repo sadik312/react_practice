6f5e22c825c507e3b634a0bd2f683fdd1d847062
Sadik <sadikislam46@gmail.com>
2023-09-12 19:40:13 +0100

    adding list component

---
+++ react-tutorial/src/Components/data.jsx
@@ -0,0 +1,16 @@
+export const people = [{
+    id: 0,
+    name: 'Creola Katherin Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A'
+}, {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa'
+}, {
+    
+}
+];
