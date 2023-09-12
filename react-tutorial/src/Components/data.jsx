c503c65cfa4c74a4d44368b4d69533e5abc2565f
Sadik <sadikislam46@gmail.com>
2023-09-12 20:49:13 +0100

    added data to data.jsx

--- react-tutorial/src/Components/data.jsx
+++ react-tutorial/src/Components/data.jsx
@@ -11,6 +11,16 @@ export const people = [{
     accomplishment: 'discovery of Arctic ozone hole',
     imageId: 'mynHUSa'
 }, {
-    
+   id: 2,
+   name: 'Mohammad Abdus Salam',
+   profession: 'physicist',
+   accomplishment: 'electromagnetism theory',
+   imageId: 'IOjWm71' 
+}, {
+    id: 3,
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',
+    accomplishment: 'pioneering cortisone drugs, steriods and birth control pills',
+    imageId: 'be7W1ji'
 }
 ];
